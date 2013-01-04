a6479f682e5dc3840492671fc54ddb7d8c0252ed
wrathematics <wrathematics@gmail.com>
2013-01-04 13:57:05 -0500

    01.04.13 nightly

--- R/ddmatrix_condition.R
+++ R/ddmatrix_condition.R
@@ -1,36 +1,83 @@
-# kappa.ddmatrix source lifted heavily from R's kappa.default function
+# kappa* sources lifted heavily from R's kappa.default function
+
+###kappa.ddmatrix <- function (z, exact = FALSE, norm = NULL, method = c("qr", "direct"), ...) 
+###{
+###  method <- match.arg(method)
+###  
+###  if (!is.null(norm))
+###    norm <- match.arg(norm, c("2", "1", "O", "I"))
+###  else 
+###    norm <- "2"
+###  
+###  if (exact && norm == "2") {
+###    s <- svd(z, nu = 0, nv = 0)$d
+###    ret <- max(s)/min(s[s > 0])
+###  } else {
+###    if (exact)
+###      warning(gettextf("norm '%s' currently always uses exact = FALSE", norm))
+###    if (norm=="2")
+###      norm <- "O"
+###    
+###    d <- z@dim
+###    
+###    if (method == "qr" || d[1L] != d[2L])
+###      z <- qr.R(qr(if (d[1L] < d[2L]) t(z) else z), complete=TRUE)
+###    
+###    if (norm=="I")
+###      ret <- 1/base.rpdtrcon(x=z, type=norm, uplo="L")
+###    else
+###      ret <- 1/base.rpdtrcon(x=z, type=norm, uplo="U")
+###  }
+###  
+###  return( ret )
+###}
+
+.kappa_tri2 <- function (z, exact = FALSE, norm = NULL, ...) 
+{
+  if (exact) {
+    stopifnot(is.null(norm) || identical("2", norm))
+    kappa.default(z, exact = TRUE)
+  } else {
+    p <- as.integer(nrow(z))
+    if (is.na(p)) 
+        stop("invalid nrow(x)")
+    if (p != ncol(z)) 
+        stop("triangular matrix should be square")
+    if (is.null(norm)) 
+        norm <- "1"
+    else 
+        1/base.rpdtrcon(x=z, type=norm, uplo="U")
+  }
+}
+
+kappa.qr2 <- function (z, ...) 
+{
+    R <- qr.R(z, complete=FALSE)
+    .kappa_tri2(R, ...)
+}
+
 kappa.ddmatrix <- function (z, exact = FALSE, norm = NULL, method = c("qr", "direct"), ...) 
 {
   method <- match.arg(method)
-  
-  if (!is.null(norm)) 
-    norm <- match.arg(norm, c("2", "1", "O", "I"))
-  else 
-    norm <- "2"
-  
+  norm <- if (!is.null(norm)) 
+            match.arg(norm, c("2", "1", "O", "I"))
+          else 
+            "2"
   if (exact && norm == "2") {
     s <- svd(z, nu = 0, nv = 0)$d
-    ret <- max(s)/min(s[s > 0])
+    max(s)/min(s[s > 0])
   } else {
-    if (exact)
+    if (exact) 
       warning(gettextf("norm '%s' currently always uses exact = FALSE", norm))
     if (norm=="2")
       norm <- "O"
-    
-    d <- z@dim
-    
-    if (method == "qr" || d[1L] != d[2L])
-      z <- qr.R(qr(if (d[1L] < d[2L]) t(z) else z), complete=TRUE)
-    
-    if (norm=="I")
-      ret <- 1/base.rpdtrcon(x=z, type=norm, uplo="L")
-    else
-      ret <- 1/base.rpdtrcon(x=z, type=norm, uplo="U")
+    d <- dim(z)
+    if (method == "qr" || d[1L] != d[2L]) 
+      kappa.qr2(qr(if (d[1L] < d[2L]) t(z) else z), exact = FALSE, norm = norm, ...)
+    else 
+      .kappa_tri2(z, exact = FALSE, norm = norm, ...)
   }
-  
-  return( ret )
 }
-
 
 
 setMethod("rcond", signature(x="ddmatrix"),
--- R/zzz.R
+++ R/zzz.R
@@ -1,23 +1,36 @@
-.First.lib <- function(lib, pkg){
+### Lastest load into a package.
+
+### Export Namespace does not use .First.lib() and .Last.lib(), but use
+### .onLoad() and .onUnload().
+# .First.lib <- function(lib, pkg){
+# } # End of .First.lib().
+
+# .Last.lib <- function(libpath){
+# } # End of .Last.lib().
+
+.onLoad <- function(libname, pkgname){
   if(! is.loaded("spmd_initialize")){
-    library.dynam("pbdMPI", "pbdMPI", lib)
+    library.dynam("pbdMPI", "pbdMPI", libname)
     if(pbdMPI:::comm.is.null(0L) == -1){
       pbdMPI:::init()
     }
   }
 
   if(! is.loaded("slap_blacs_gridinit")){
-    library.dynam("pbdSLAP", "pbdSLAP", lib)
+    library.dynam("pbdSLAP", "pbdSLAP", libname)
   }
 
   if(! is.loaded("mpi_blacs_initialize")){
-    library.dynam("pbdBASE", "pbdBASE", lib)
+    library.dynam("pbdBASE", "pbdBASE", libname)
   }
 
-  library.dynam("pbdDMAT", pkg, lib)
-} # End of .First.lib().
+  library.dynam("pbdDMAT", pkgname, libname)
+  invisible()
+} # End of .onload().
 
-.Last.lib <- function(libpath){
+.onUnload <- function(libpath){
   library.dynam.unload("pbdDMAT", libpath)
-} # End of .Last.lib().
+  invisible()
+} # End of .onUnload().
+
 
