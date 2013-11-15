4b8bb711e677f8b5c4e45433f71bcf8fac620021
wrathematics <wrathematics@gmail.com>
2013-11-15 16:51:48 -0500

    11.15.13 moved everything again

--- src/matexp.h
+++
@@ -1,9 +0,0 @@
-#ifndef DMAT_MATEXP_H
-#define DMAT_MATEXP_H
-
-
-void dmat_matexp_pade(const unsigned int n, double *A, double *N, double *D);
-void matpow_by_squaring(double *A, int n, int b, double *P);
-
-
-#endif
