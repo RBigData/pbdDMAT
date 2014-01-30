41b5ad54b9a0f1ae6bb6895c044824957b09a594
wrathematics <wrathematics@gmail.com>
2014-01-30 14:06:37 -0500

    dsmat stuff working; added %*%

--- R/dsmatrix_algebra.r
+++ R/dsmatrix_algebra.r
@@ -20,3 +20,35 @@ setMethod("t", signature(x="dsmatrix"),
   }
 )
 
+
+setMethod("%*%", signature(x="dsmatrix", y="dsmatrix"),
+  function(x, y)
+  {
+    # Matrix multiplication dimension crap
+    if (x@dim[2L] != y@dim[1L])
+      comm.stop("Error : non-conformable arguments.")
+    
+    m <- x@dim[1L]
+    n <- y@dim[2L]
+    
+    new_dim <- c(m, n)
+    new_ldim <- c(dmat_ldim(m), n)
+    
+    
+    # Compute product
+    out <- petsc_matmatmult(A_dim=x@dim, A_ldim=x@ldim, A_data=x@Data, A_row_ptr=x@row_ptr, A_col_ind=x@col_ind, 
+                            B_dim=y@dim, B_ldim=y@ldim, B_data=y@Data, B_row_ptr=y@row_ptr, B_col_ind=y@col_ind)
+    
+    
+    # wrangle return
+    if (new_ldim[1L] == 0)
+      Data <- matrix(0.0, 1L, 1L)
+    else
+      Data <- out$Data
+    
+    ret <- new("dsmatrix", dim=new_dim, ldim=new_ldim, Data=Data, row_ptr=out$row_ptr, col_ind=out$col_ind)
+    
+    return( ret )
+  }
+)
+
