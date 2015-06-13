b8215fda2e72a39b1be7d4c864eed359124a1efd
Drew Schmidt <schmidt@math.utk.edu>
2015-06-12 20:50:04 -0400

    moved chol

--- R/ddmatrix_algebra.R
+++ R/ddmatrix_algebra.R
@@ -72,40 +72,6 @@ NULL
 
 
 
-# ################################################
-# ------------------------------------------------
-# Matrix Factorizations
-# ------------------------------------------------
-# ################################################
-
-
-
-
-
-#' @rdname linalg
-#' @export
-setMethod("chol", signature(x="ddmatrix"), 
-  function(x)
-  {
-    if (diff(x@dim)!=0)
-      comm.stop(paste("'x' (", x@dim[1L], " x ", x@dim[2L], ") must be square", sep=""))
-    
-    desca <- base.descinit(dim=x@dim, bldim=x@bldim, ldim=x@ldim, ICTXT=x@ICTXT)
-    
-    n <- desca[4L]
-    
-    uplo <- "U"
-    
-    out <- base.rpdpotrf(uplo=uplo, n=n, a=x@Data, desca=desca)
-    
-    ret <- new("ddmatrix", Data=out$A, dim=x@dim, ldim=x@ldim, bldim=x@bldim, ICTXT=x@ICTXT)
-    
-    ret@Data <- base.tri2zero(x=ret@Data, descx=desca, uplo='L', diag='N')
-    
-    return( ret )
-  }
-)
-
 
 
 
---
+++ R/ddmatrix_chol.r
@@ -0,0 +1,58 @@
+#' Cholesky Factorization
+#' 
+#' Cholesky factorization for distributed matrices with R-like syntax, with
+#' calculations performed by the PBLAS and ScaLAPACK libraries.
+#' 
+#' Extensions of R linear algebra functions.
+#' 
+#' @param x
+#' numeric distributed matrices.
+#' 
+#' @return 
+#' \code{chol()} performs Cholesky factorization.
+#' 
+#' @examples
+#' \dontrun{
+#' # Save code in a file "demo.r" and run with 2 processors by
+#' # > mpiexec -np 2 Rscript demo.r
+#' 
+#' library(pbdDMAT, quiet = TRUE)
+#' init.grid()
+#' 
+#' # don't do this in production code
+#' x <- matrix(1:9, 3)
+#' x <- as.ddmatrix(x)
+#' 
+#' y <- solve(t(A) %*% A)
+#' print(y)
+#' 
+#' finalize()
+#' }
+#' 
+#' @keywords Methods Linear Algebra
+#' @name ddmatrix-chol
+#' @rdname ddmatrix-chol
+#' @export
+setMethod("chol", signature(x="ddmatrix"), 
+  function(x)
+  {
+    if (diff(x@dim)!=0)
+      comm.stop(paste("'x' (", x@dim[1L], " x ", x@dim[2L], ") must be square", sep=""))
+    
+    desca <- base.descinit(dim=x@dim, bldim=x@bldim, ldim=x@ldim, ICTXT=x@ICTXT)
+    
+    n <- desca[4L]
+    
+    uplo <- "U"
+    
+    out <- base.rpdpotrf(uplo=uplo, n=n, a=x@Data, desca=desca)
+    
+    ret <- new("ddmatrix", Data=out$A, dim=x@dim, ldim=x@ldim, bldim=x@bldim, ICTXT=x@ICTXT)
+    
+    ret@Data <- base.tri2zero(x=ret@Data, descx=desca, uplo='L', diag='N')
+    
+    return( ret )
+  }
+)
+
+
