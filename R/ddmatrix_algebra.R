9ec9b17a77a7bbb1877154cc9ebd811b0ffd2599
Drew Schmidt <schmidt@math.utk.edu>
2015-06-12 20:44:17 -0400

    moved solve

--- R/ddmatrix_algebra.R
+++ R/ddmatrix_algebra.R
@@ -5,10 +5,8 @@
 #' 
 #' Extensions of R linear algebra functions.
 #' 
-#' @param x,a,b
-#' numeric distributed matrices.  If applicable, \code{a}
-#' and \code{b} must be on the same BLACS context and have the same blocking
-#' dimension.
+#' @param x
+#' numeric distributed matrices.
 #' @param ... 
 #' additional arguments.
 #' @param symmetric 
@@ -71,51 +69,6 @@
 #' @name linlalg
 #' @rdname linalg
 NULL
-
-
-# inversion
-#' @rdname linalg
-#' @export
-setMethod("solve", signature(a="ddmatrix"), 
-  function(a)
-  {
-    if (diff(a@dim)!=0)
-      comm.stop(paste("'a' (", a@dim[1], " x ", a@dim[2], ") must be square", sep=""))
-    
-    desca <- base.descinit(dim=a@dim, bldim=a@bldim, ldim=a@ldim, ICTXT=a@ICTXT)
-    
-    n <- desca[4L]
-    
-    out <- base.rpdgetri(n=n, a=a@Data, desca=desca)
-    
-    a@Data <- out
-    
-    return(a)
-  }
-)
-
-# Solving systems
-#' @rdname linalg
-#' @export
-setMethod("solve", signature(a="ddmatrix", b="ddmatrix"), 
-  function(a, b)
-  {
-    base.checkem(x=a, y=b, checks=2:3)
-    
-    # Matrix descriptors
-    desca <- base.descinit(dim=a@dim, bldim=a@bldim, ldim=a@ldim, ICTXT=a@ICTXT)
-    descb <- base.descinit(dim=b@dim, bldim=b@bldim, ldim=b@ldim, ICTXT=b@ICTXT)
-    
-    n <- desca[4L]
-    nrhs <- descb[4L]
-    
-    ret <- base.rpdgesv(n=n, nrhs=nrhs, a=a@Data, desca=desca, b=b@Data, descb=descb)
-    
-    b@Data <- ret
-    
-    return( b )
-  }
-)
 
 
 
