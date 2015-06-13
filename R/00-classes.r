b7502dc6a8cd78771ca100fd300ae37063a1bb9f
Drew Schmidt <schmidt@math.utk.edu>
2015-06-12 21:33:51 -0400

    spacing

--- R/00-classes.r
+++ R/00-classes.r
@@ -108,22 +108,22 @@ setClass(
 
 # Distributed Dense Vector
 setClass(
-          Class="ddvector", 
-          representation=representation(
-                         Data="vector",
-                         len="numeric",
-                         llen="numeric",
-                         bldim="numeric",
-                         ICTXT="numeric"
-          ),
-          
-          prototype=prototype(
-                         Data=0.0,
-                         len=1L,
-                         llen=1L,
-                         bldim=c(1L, 1L),
-                         ICTXT=0L
-          )
+  Class="ddvector", 
+  representation=representation(
+     Data="vector",
+     len="numeric",
+     llen="numeric",
+     bldim="numeric",
+     ICTXT="numeric"
+  ),
+  
+  prototype=prototype(
+    Data=0.0,
+    len=1L,
+    llen=1L,
+    bldim=c(1L, 1L),
+    ICTXT=0L
+  )
 )
 
 
