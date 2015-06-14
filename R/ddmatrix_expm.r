29e72f8ff25d17c5dd75ce86b6119daac932a50d
Drew Schmidt <schmidt@math.utk.edu>
2015-06-13 20:28:14 -0400

    fixed expm examplesR

--- R/ddmatrix_expm.r
+++ R/ddmatrix_expm.r
@@ -26,6 +26,18 @@
 #' @return 
 #' Returns a distributed matrix.
 #' 
+#' @examples
+#' \dontrun{
+#' # Save code in a file "demo.r" and run with 2 processors by
+#' # > mpiexec -np 2 Rscript demo.r
+#' 
+#' library(pbdDMAT, quiet = TRUE)
+#' init.grid()
+#' 
+#' x <- matrix("rnorm", 5, 5, bldim=2)
+#' expm(x)
+#' 
+#' }
 #' @keywords Methods Linear Algebra
 #' @name expm
 #' @rdname expm
